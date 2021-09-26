43dd14ee2eaf9aad0f0ff3ec14a4b3067ed59ced
Xypter <xypter@gmail.com>
2021-09-25 21:35:44 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../src/css/icon/icon.css';
+import '../src/css/mfzicon/mfzicon.css';
+import '../src/css/hpz.css';
+import '../src/css/mfz.css';
+import '../src/css/ssz.css';
+import '../src/css/style_v7.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
