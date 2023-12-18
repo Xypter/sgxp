8600d8bc5ad250408236dd86531061488a58538b
Xypter <xypter@gmail.com>
2023-12-18 20:55:38 -0300

    Updates and fixes

---
+++ visible-venus/src/js/font.js
@@ -0,0 +1,11 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const TextToSVG = require('text-to-svg');
+const textToSVG = TextToSVG.loadSync();
+ 
+const attributes = {fill: 'red', stroke: 'black'};
+const options = {x: 0, y: 0, fontSize: 72, anchor: 'top', attributes: attributes};
+ 
+const svg = textToSVG.getSVG('hello', options);
+ 
+console.log(svg);
