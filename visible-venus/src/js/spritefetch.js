8ba0a12fa2bb6be9acd00b677328890f5027be11
Xypter <xypter@gmail.com>
2024-01-08 20:19:12 -0300

    experiment with astro slug

--- visible-venus/src/js/spritefetch.js
+++ visible-venus/src/js/spritefetch.js
@@ -28,7 +28,7 @@ function getSprites() {
       data.data.forEach(sprite => {
         div.innerHTML += `
 
-    <a href="../sheets/${sprite.id}" class="sprite-box">
+    <a href="${sprite.attributes.slug}" class="sprite-box">
       <div class="sprite-star-container">
         <div class="sprite-star"></div>
         <div class="sprite-star"></div>
