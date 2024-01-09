fbb292e54cc2a3d054a4f114d8012f6960ca4d2c
Xypter <xypter@gmail.com>
2024-01-09 16:50:04 -0300

    Views now included in fetch request. Umami update

--- visible-venus/src/js/spritefetch.js
+++ visible-venus/src/js/spritefetch.js
@@ -1,3 +1,8 @@
+const umamiKey = import.meta.env.PUBLIC_UMAMI_TOKEN;
+const todaysDate = Date.now();
+const localhost = "http://localhost:1337/api/sprites?populate=*"
+const apiUrl = "https://api.sgxp.me/api/sprites?[populate]=*"
+
 function count(number) {
   if (number <= 9) {
     return '00' + number
@@ -20,13 +25,40 @@ function formatBytes(bytes, decimals = 2) {
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
+
+// function getStats() {
+//   fetch(`https://analytics.sgxp.me/api/websites/c78c6fb7-bd5f-4715-8af5-f794ad7b3584/stats?startAt=1672578061000&endAt=${todaysDate}&url=/sprites`, {
+//   headers: {Authorization: `Bearer ${umamiKey}`}
+// })
+//    .then(res => res.json())
+//    .then(data => console.log(data.pageviews.value))
+// }
+
+// getStats()
+
+
 function getSprites() {
+  fetch(`https://analytics.sgxp.me/api/websites/c78c6fb7-bd5f-4715-8af5-f794ad7b3584/stats?startAt=1672578061000&endAt=${todaysDate}&url=/sprites`, {
+    headers: {Authorization: `Bearer ${umamiKey}`}
+  })
+  .then(res => res.json())
+  .then(data => {
+    let views = data.pageviews.value
+  })
+
     const div = document.getElementById('hello')
-    fetch('https://api.sgxp.me/api/sprites?[populate]=*')
+    fetch(apiUrl)
     .then(res => res.json())
     .then(data => {
       data.data.forEach(sprite => {
-        div.innerHTML += `
+        fetch(`https://analytics.sgxp.me/api/websites/c78c6fb7-bd5f-4715-8af5-f794ad7b3584/stats?startAt=1672578061000&endAt=${todaysDate}&url=/sprites/${sprite.id}`, {
+    headers: {Authorization: `Bearer ${umamiKey}`}
+  })
+  .then(res => res.json())
+  .then(data => {
+    let views = data.pageviews.value
+
+    div.innerHTML += `
 
     <a href="/sprites/${sprite.id}" class="sprite-box">
       <div class="sprite-star-container">
@@ -58,7 +90,7 @@ function getSprites() {
         <div class="sprite-text">${sprite.attributes.createdBy.username}</div>
       </div>
       <div class="sprite-stats">
-        <div class="sprite-text"></div>
+        <div class="sprite-text">${views}</div>
       </div>
       <div class="sprite-stats">
         <div class="sprite-text">${formatBytes(sprite.attributes.spritesheet.data.attributes.size, 2)}</div>
@@ -66,6 +98,8 @@ function getSprites() {
     </a>
 
         `
+  })
+        
       })
     })
   }
