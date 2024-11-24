9ec489681c5cfaf3fb8993c327e6d93d7a57178a
Xypter <xypter@gmail.com>
2024-11-23 22:30:37 -0300

    edit

--- visible-venus/astro.config.mjs
+++ visible-venus/astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
-  vite: {
-    ssr: {
-      noExternal: ['path-to-regexp'],
-    },
-  },
+  adapter: node({
+    mode: 'middleware',
+  }),
 });
