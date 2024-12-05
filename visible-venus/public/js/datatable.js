4932a37f7e513c9152e0c2cdb451777f7a34d0fc
Xypter <xypter@gmail.com>
2024-12-04 23:02:28 -0300

    archive update

--- visible-venus/public/js/datatable.js
+++ visible-venus/public/js/datatable.js
@@ -1,46 +1,74 @@
 var dataSet = [
-    ["10005", "Halo Online", "Halo", "301", "291", "0.97", "Nothing to write home about", "4", "Comic with occasional mix of sprites and memes. Group comic", "/jeevespage?comic_id=10005"],
-["10065", "SoniX", "Sonic", "10", "10", "1", "Questioned my sanity archiving this", "0", "Pretty much only put this here cause it had Sonic in it", "/jeevespage?comic_id=10065"],
-["10070", "RuneScape Misadventures", "Random", "20", "20", "1", "For historical purposes only", "1", "Runescape", "/jeevespage?comic_id=10070"],
-["10185", "Authortastic 2.0", "Sonic", "301", "283", "0.94", "Nothing to write home about", "4", "Looks like a group comic, so quality flactuates. Has interesting character designs (mostly recolors lol) though and the authors seem to get better over time.", "/jeevespage?comic_id=10185"],
-["10203", "What Ever Sprite Comic", "Sonic", "7", "7", "1", "Solid title", "8", "Comic tutorials by YoshiRyu, so this is a gem", "/jeevespage?comic_id=10203"],
-["10208", "Mission Complete", "Mixed", "28", "28", "1", "Sloppy", "3", "Starfox with a mix of Mario and other characters.", "/jeevespage?comic_id=10208"],
-["10224", "No Bgs ReNew'd!", "Sonic", "111", "107", "0.96", "Interesting", "6", "Mix of decent and horrible. Comics posted are decent, with a mix of memes. Higher rating since it has screenshots with historical value (See chapter 107)", "/jeevespage?comic_id=10224"],
-["10413", "Sonic Realities", "Sonic", "6", "6", "1", "Sloppy", "3", "Pretty short fan character comic", "/jeevespage?comic_id=10413"],
-["10506", "My Old Comic", "Sonic", "38", "33", "0.87", "What am I reading right now", "2", "Sally was a slut. Page 3. LOL", "/jeevespage?comic_id=10506"],
-["10514", "The Mind of sc276", "Sonic", "122", "120", "0.98", "For historical purposes only", "1", "Quality is kinda yuck but seems interesting", "/jeevespage?comic_id=10514"],
-["10676", "The Three of Chaos", "Sonic", "17", "10", "0.59", "For historical purposes only", "1", "Missing half the pages", "/jeevespage?comic_id=10676"],
-["62037", "Dead Indeed", "Sonic", "83", "80", "0.96", "Classic Status", "10", "Dead Indeed", "/jeevespage?comic_id=62037"],
-["100034", "A New Adventure", "Sonic", "8", "8", "1", "Sloppy", "3", "Some OC comic. Neat sprites", "/jeevespage?comic_id=100034"],
-["100160", "another Pokemon mystery dungeon author comic", "Pokemon", "79", "76", "0.96", "For historical purposes only", "1", "Sprite Compilation with a comic halfway through", "/jeevespage?comic_id=100160"],
-["100206", "The Blue Friendship", "Sonic", "23", "22", "0.96", "Nothing to write home about", "4", "Sonic goes to school and misses french class lol", "/jeevespage?comic_id=100206"],
-["100209", "Author/Recolor mystery dungeon (Co-Author comic)", "Mixed", "24", "24", "1", "What am I reading right now", "2", "Random mix of sprite sheets and comics", "/jeevespage?comic_id=100209"],
-["100304", "Tappa's Questions And Answers", "Megaman", "16", "16", "1", "Sloppy", "3", "Character answers questions from the audience", "/jeevespage?comic_id=100304"],
-["100464", "Yoshi & Kirby", "Mixed", "109", "106", "0.97", "Passable", "5", "Both Yoshi's and Kirby's in the same world. Comic has a consistent style and a decent length.", "/jeevespage?comic_id=100464"],
-["100505", "All There Was", "Mario", "55", "55", "1", "Passable", "5", "Not a sprite comic. Decent art quality. Doesn't involve Mario but does have side characters from Mario", "/jeevespage?comic_id=100505"],
-["100531", ".:Nothing But Random:.", "Sonic", "8", "7", "0.88", "Sloppy", "3", "Person might be from MGComics?", "/jeevespage?comic_id=100531"],
-["100564", "CPU Switch", "Sonic", "43", "39", "0.91", "For historical purposes only", "1", "Sprite Compilation. Has some sprite assets people might find interesting.", "/jeevespage?comic_id=100564"],
-["100612", ".:NONSENSE:.", "Sonic", "8", "7", "0.88", "What am I reading right now", "2", "Short group comic", "/jeevespage?comic_id=100612"],
-["100615", "The Inferno chronicles", "Sonic", "6", "6", "1", "For historical purposes only", "1", "This person really liked gradients and inverted colors lol", "/jeevespage?comic_id=100615"],
-["100628", "From Hell and Beyond Sprite Shack", "Sonic", "11", "10", "0.91", "For historical purposes only", "1", "Sprite Compilation with fan characters", "/jeevespage?comic_id=100628"],
-["100666", "NoPUNintendo", "Mario", "44", "40", "0.91", "Classic Status", "10", "Nopunintendo.net. On the same quality as GG Guys", "/jeevespage?comic_id=100666"],
-["100681", "1ce k1d's museum of really crazy stuff", "Random", "269", "267", "0.99", "For historical purposes only", "1", "Random Sprite Compilation", "/jeevespage?comic_id=100681"],
-["100712", "Dragon Quest Gaiden: The Crater of the Earth", "Random", "9", "9", "1", "Sloppy", "3", "Dragon Quest", "/jeevespage?comic_id=100712"],
-["100779", "Rose's Garden.", "Sonic", "10", "10", "1", "For historical purposes only", "1", "Super Random", "/jeevespage?comic_id=100779"],
-["100792", "The Adventures of LR Fan Base", "Sonic", "66", "65", "0.98", "For historical purposes only", "1", "Sprite Compilation with some half decent fan characters", "/jeevespage?comic_id=100792"],
-["100856", "Smashfan's World Conquest", "Mixed", "73", "73", "1", "Nothing to write home about", "4", "Lot of mixels in this one, but I give them props for keeping the formatting consistent at least…", "/jeevespage?comic_id=100856"],
-["100910", "KD'S Pokemon Journey", "Pokemon", "5", "5", "1", "For historical purposes only", "1", "Super short", "/jeevespage?comic_id=100910"],
-["100956", "Super Sonic Comic: Act 3", "Sonic", "23", "23", "1", "Pretty Good", "7", "Very obviously inspired by InSonicnia. A shame it's so short", "/jeevespage?comic_id=100956"],
-["101088", "Cyren's Showcase/Requested Sprites", "Sonic", "102", "99", "0.97", "For historical purposes only", "1", "Sprite Compilation", "/jeevespage?comic_id=101088"],
-["101119", "Sprite Vacation Hotel 2", "Sonic", "964", "907", "0.94", "Questioned my sanity archiving this", "0", "Sprite Compilation. People posting pictures of themselves for whatever reason lol", "/jeevespage?comic_id=101119"],
-["101265", "MSMN's castle for random stuff", "Sonic", "21", "16", "0.76", "For historical purposes only", "1", "Sprite Compilation. Sprites of Neox with a tail lol", "/jeevespage?comic_id=101265"],
-["101272", "The ACA Comics", "Mixed", "39", "39", "1", "What am I reading right now", "2", "Mixed bag with consistent style.", "/jeevespage?comic_id=101272"],
+    ["10005", "GreyouTT", "Halo Online", "Halo", "301", "291", "0.97", "Nothing to write home about", "4", "Comic with occasional mix of sprites and memes. Group comic", "/jeevespage?comic_id=10005"],
+["10065", "Maker", "SoniX", "Sonic", "10", "10", "1", "Questioned my sanity archiving this", "0", "Pretty much only put this here cause it had Sonic in it", "/jeevespage?comic_id=10065"],
+["10070", "Manuel176", "RuneScape Misadventures", "Random", "20", "20", "1", "For historical purposes only", "1", "Runescape", "/jeevespage?comic_id=10070"],
+["10185", "supercomputer276", "Authortastic 2.0", "Sonic", "301", "283", "0.94", "Nothing to write home about", "4", "Looks like a group comic, so quality flactuates. Has interesting character designs (mostly recolors lol) though and the authors seem to get better over time.", "/jeevespage?comic_id=10185"],
+["10203", "YoshyRyuDCC", "What Ever Sprite Comic", "Sonic", "7", "7", "1", "Solid title", "8", "Comic tutorials by YoshiRyu, so this is a gem", "/jeevespage?comic_id=10203"],
+["10208", "pimp_named_slinkback", "Mission Complete", "Mixed", "28", "28", "1", "Sloppy", "3", "Starfox with a mix of Mario and other characters.", "/jeevespage?comic_id=10208"],
+["10224", "Xavier the Fox", "No Bgs ReNew'd!", "Sonic", "111", "107", "0.96", "Interesting", "6", "Mix of decent and horrible. Comics posted are decent, with a mix of memes. Higher rating since it has screenshots with historical value (See chapter 107)", "/jeevespage?comic_id=10224"],
+["10413", "Pokeshadow", "Sonic Realities", "Sonic", "6", "6", "1", "Sloppy", "3", "Pretty short fan character comic", "/jeevespage?comic_id=10413"],
+["10506", "Entoxica", "My Old Comic", "Sonic", "38", "33", "0.87", "What am I reading right now", "2", "Sally was a slut. Page 3. LOL", "/jeevespage?comic_id=10506"],
+["10514", "supercomputer276", "The Mind of sc276", "Sonic", "122", "120", "0.98", "For historical purposes only", "1", "Quality is kinda yuck but seems interesting", "/jeevespage?comic_id=10514"],
+["10676", "Flame_The_Hybrid", "The Three of Chaos", "Sonic", "17", "10", "0.59", "For historical purposes only", "1", "Missing half the pages", "/jeevespage?comic_id=10676"],
+["62037", "Lyoko the Newt", "Dead Indeed", "Sonic", "83", "80", "0.96", "Classic Status", "10", "Dead Indeed", "/jeevespage?comic_id=62037"],
+["100034", "Hazel The Hedgehog", "A New Adventure", "Sonic", "8", "8", "1", "Sloppy", "3", "Some OC comic. Neat sprites", "/jeevespage?comic_id=100034"],
+["100160", "Shade2470", "another Pokemon mystery dungeon author comic", "Pokemon", "79", "76", "0.96", "For historical purposes only", "1", "Sprite Compilation with a comic halfway through", "/jeevespage?comic_id=100160"],
+["100206", "Hero of Comedy", "The Blue Friendship", "Sonic", "23", "22", "0.96", "Nothing to write home about", "4", "Sonic goes to school and misses french class lol", "/jeevespage?comic_id=100206"],
+["100209", "Zekrom123", "Author/Recolor mystery dungeon (Co-Author comic)", "Mixed", "24", "24", "1", "What am I reading right now", "2", "Random mix of sprite sheets and comics", "/jeevespage?comic_id=100209"],
+["100304", "Alienoid", "Tappa's Questions And Answers", "Megaman", "16", "16", "1", "Sloppy", "3", "Character answers questions from the audience", "/jeevespage?comic_id=100304"],
+["100464", "Ultimate Yoshi", "Yoshi & Kirby", "Mixed", "109", "106", "0.97", "Passable", "5", "Both Yoshi's and Kirby's in the same world. Comic has a consistent style and a decent length.", "/jeevespage?comic_id=100464"],
+["100505", "SelanPike", "All There Was", "Mario", "55", "55", "1", "Passable", "5", "Not a sprite comic. Decent art quality. Doesn't involve Mario but does have side characters from Mario", "/jeevespage?comic_id=100505"],
+["100531", "DelSoul", ".:Nothing But Random:.", "Sonic", "8", "7", "0.88", "Sloppy", "3", "Person might be from MGComics?", "/jeevespage?comic_id=100531"],
+["100564", "jameswolf100", "CPU Switch", "Sonic", "43", "39", "0.91", "For historical purposes only", "1", "Sprite Compilation. Has some sprite assets people might find interesting.", "/jeevespage?comic_id=100564"],
+["100612", "Faekmastah", ".:NONSENSE:.", "Sonic", "8", "7", "0.88", "What am I reading right now", "2", "Short group comic", "/jeevespage?comic_id=100612"],
+["100615", "sonicfan42", "The Inferno chronicles", "Sonic", "6", "6", "1", "For historical purposes only", "1", "This person really liked gradients and inverted colors lol", "/jeevespage?comic_id=100615"],
+["100628", "Master J M13", "From Hell and Beyond Sprite Shack", "Sonic", "11", "10", "0.91", "For historical purposes only", "1", "Sprite Compilation with fan characters", "/jeevespage?comic_id=100628"],
+["100666", "TerminalMontage", "NoPUNintendo", "Mario", "44", "40", "0.91", "Classic Status", "10", "Nopunintendo.net. On the same quality as GG Guys", "/jeevespage?comic_id=100666"],
+["100681", "1ce_k1d", "1ce k1d's museum of really crazy stuff", "Random", "269", "267", "0.99", "For historical purposes only", "1", "Random Sprite Compilation", "/jeevespage?comic_id=100681"],
+["100712", "TDB", "Dragon Quest Gaiden: The Crater of the Earth", "Random", "9", "9", "1", "Sloppy", "3", "Dragon Quest", "/jeevespage?comic_id=100712"],
+["100779", "Amy Rose the Hedehog", "Rose's Garden.", "Sonic", "10", "10", "1", "For historical purposes only", "1", "Super Random", "/jeevespage?comic_id=100779"],
+["100792", "Bre Ishurna the Wolf", "The Adventures of LR Fan Base", "Sonic", "66", "65", "0.98", "For historical purposes only", "1", "Sprite Compilation with some half decent fan characters", "/jeevespage?comic_id=100792"],
+["100856", "Smashfan64", "Smashfan's World Conquest", "Mixed", "73", "73", "1", "Nothing to write home about", "4", "Lot of mixels in this one, but I give them props for keeping the formatting consistent at least…", "/jeevespage?comic_id=100856"],
+["100910", "PKNESS", "KD'S Pokemon Journey", "Pokemon", "5", "5", "1", "For historical purposes only", "1", "Super short", "/jeevespage?comic_id=100910"],
+["100956", "METAL SHADOWmk80", "Super Sonic Comic: Act 3", "Sonic", "23", "23", "1", "Pretty Good", "7", "Very obviously inspired by InSonicnia. A shame it's so short", "/jeevespage?comic_id=100956"],
+["101088", "SonicWad", "Cyren's Showcase/Requested Sprites", "Sonic", "102", "99", "0.97", "For historical purposes only", "1", "Sprite Compilation", "/jeevespage?comic_id=101088"],
+["101119", "13thecat", "Sprite Vacation Hotel 2", "Sonic", "964", "907", "0.94", "Questioned my sanity archiving this", "0", "Sprite Compilation. People posting pictures of themselves for whatever reason lol", "/jeevespage?comic_id=101119"],
+["101265", "mariosonicmegaman", "MSMN's castle for random stuff", "Sonic", "21", "16", "0.76", "For historical purposes only", "1", "Sprite Compilation. Sprites of Neox with a tail lol", "/jeevespage?comic_id=101265"],
+["101272", "Luigifan2000", "The ACA Comics", "Mixed", "39", "39", "1", "What am I reading right now", "2", "Mixed bag with consistent style.", "/jeevespage?comic_id=101272"],
+["101433", "yugi67", "Pokemon Voyagers", "Pokemon", "9", "9", "1", "Passable", "5", "Went on hiatus pretty quickly lol", "/jeevespage?comic_id=101433"],
+["101468", "Pengijoker", "A KaTAM Comik (More about the Mirror World!)", "Kirby", "13", "13", "1", "What am I reading right now", "2", "Incredibly random", "/jeevespage?comic_id=101468"],
+["101524", "YoshyRyuDCC", "TSZ Weekly", "Sonic", "5", "5", "1", "Interesting", "6", "Looks to be a tutorial comic", "/jeevespage?comic_id=101524"],
+["101706", "Gehshi Tyukinomi", "Rockhorn Academy", "Sonic", "12", "12", "1", "For historical purposes only", "1", "Fan character goes to academy. The end", "/jeevespage?comic_id=101706"],
+["101787", "darkin1998", "Around the world in 8-bit days", "Mario", "11", "11", "1", "For historical purposes only", "1", "Lots of white space", "/jeevespage?comic_id=101787"],
+["101802", "Scrape the Wolf", "Sonic Blitz", "Sonic", "23", "22", "0.96", "Nothing to write home about", "4", "Interesting character sprites though :0", "/jeevespage?comic_id=101802"],
+["101912", "Extreme5000", "Sonic the Hedgehog : Funny Files", "Sonic", "19", "19", "1", "For historical purposes only", "1", "Definitely made in MS Paint lol. The aspect ratios are all stretched", "/jeevespage?comic_id=101912"],
+["101992", "Zpawn", "The Empire", "Sonic", "12", "12", "1", "Passable", "5", "Decent look and style. Only 12 pages", "/jeevespage?comic_id=101992"],
+["102037", "SonicFan8690", "The Shadow's Edge", "Sonic", "6", "6", "1", "For historical purposes only", "1", "Game select comic", "/jeevespage?comic_id=102037"],
+["102071", "tekern", "Tek's sprites.", "Sonic", "32", "32", "1", "For historical purposes only", "1", "Sprite Compilation", "/jeevespage?comic_id=102071"],
+["102080", "Gigi19972010", "Kirby And The Magic Mirror", "Kirby", "45", "38", "0.84", "Pretty Good", "7", "Has a pretty consistent style through out", "/jeevespage?comic_id=102080"],
+["102081", "Gehshi Tyukinomi", "Choose your own random adventure: REVIVAL BISH!", "Sonic", "10", "10", "1", "For historical purposes only", "1", "This person just learned the word fap lol", "/jeevespage?comic_id=102081"],
+["102280", "Metalchaos0", "Mega Man: The Fan Wars", "Megaman", "8", "8", "1", "Interesting", "6", "Great aesthetic", "/jeevespage?comic_id=102280"],
+["102451", "FakeMonMaster", "Dragon Ball Sonic (Hiatus: Sunday-Following Sunday)", "Sonic", "9", "9", "1", "What am I reading right now", "2", "Uses MFZ DBZ sprites", "/jeevespage?comic_id=102451"],
+["102515", "DrizzyZX", "Digimon World X", "Sonic", "12", "12", "1", "Passable", "5", "Seems to cross-over with Digimon", "/jeevespage?comic_id=102515"],
+["102518", "Banisher_of_Sun", "my dump", "Sonic", "28", "27", "0.96", "For historical purposes only", "1", "Sprite Compilation. Some interesting designs", "/jeevespage?comic_id=102518"],
+["102537", "LuigidamanV2", "Sonic Hotel Online 4:Summer Lasts Forever?", "Sonic", "7", "7", "1", "For historical purposes only", "1", "Rooms…and sprites", "/jeevespage?comic_id=102537"],
+["102539", "hedgehog bros", "my fan characater sprites", "Sonic", "14", "14", "1", "For historical purposes only", "1", "Sprite Compilation", "/jeevespage?comic_id=102539"],
+["102550", "Ceviana", "Super Sonic Style!", "Sonic", "15", "15", "1", "Interesting", "6", "The faces in this are hilarious", "/jeevespage?comic_id=102550"],
+["102614", "Josephk", "The Attack of the Recolours Season 1", "Sonic", "119", "111", "0.93", "Passable", "5", "Has a lot of recolors but the paneling and text boxes make it a decent read", "/jeevespage?comic_id=102614"],
+["102766", "prollynotfunny", "Spacemen Go To Mars", "Random", "42", "42", "1", "For historical purposes only", "1", "Among us comic lol. probablynotfunny.com", "/jeevespage?comic_id=102766"],
+["102838", "Mijaskal", "Hello, World!", "Random", "21", "21", "1", "For historical purposes only", "1", "Random computer comic that was kinda amusing", "/jeevespage?comic_id=102838"],
+["148593", "Dregan", "Friday Night Ferocity", "Sonic", "74", "0", "0", "For historical purposes only", "1", "Nothing here unfortunately :(", "/jeevespage?comic_id=148593"],
+["149591", "Electrisa", "Pixels, Paintings and Pandemonium", "Mixed", "33", "27", "0.82", "Interesting", "6", "Some great art in this one", "/jeevespage?comic_id=149591"],
+["155906", "1337F0X", "The Purple Fox Tail", "Mixed", "19", "16", "0.84", "Interesting", "6", "Some interesting backgrounds in this one", "/jeevespage?comic_id=155906"],
+["76749", "Dregan", "Choose Your Own Bar'd", "Sonic", "26", "27", "1.04", "Solid title", "8", "A great little addition to Bar'd", "/jeevespage?comic_id=76749"],
+["81574", "Esav the Centaur", "bard future", "Sonic", "9", "9", "1", "What am I reading right now", "2", "This person was obviously a big fan of Bar'd lol", "/jeevespage?comic_id=81574"],
 
 ];
 
 var table = $('#example').DataTable({
     columns: [
         { title: 'Comic #'},
+        { title: 'Author'},
         { title: 'Title' },
         { title: 'Category'},
         { title: 'Pages in Metadata' },
@@ -64,17 +92,11 @@ var table = $('#example').DataTable({
             searchPanes: {
                 show: true
             },
-            targets: [2]
+            targets: [1]
         },
         {
             searchPanes: {
                 show: false
-            },
-            targets: [5]
-        },
-        {
-            searchPanes: {
-                show: true
             },
             targets: [6]
         },
@@ -85,7 +107,13 @@ var table = $('#example').DataTable({
             targets: [7]
         },
         {
-            targets: 5, // Index of the column to format
+            searchPanes: {
+                show: true
+            },
+            targets: [8]
+        },
+        {
+            targets: 6, // Index of the column to format
             render: function (data, type, row) {
                 // Multiply by 100 and append a percentage symbol
                 return (parseFloat(data) * 100).toFixed(0) + '%';
